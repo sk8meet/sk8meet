038cc71d0a44349aae8cc39744cf7105e56bfc06
Francois McDonald <fmcd_01@yahoo.com>
2025-03-28 06:36:59 +0000

    [TASK] Setup Jest Testing Framework for Backend Fixes #11

---
+++ backend/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.json'
+    }]
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
